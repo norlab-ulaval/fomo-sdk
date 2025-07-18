60091f67227e46538400bbc05ffef7c3debef81f
PioneerSam <pioneerqxy@gmail.com>
2025-07-18 16:04:11 -0400

    clean up comments for PR - still camera and audio remaining

--- ros2IJRR_conversion.py
+++ ros2IJRR_conversion.py
@@ -7,7 +7,6 @@ from rosbag2_py import SequentialReader, StorageOptions, ConverterOptions
 from datetime import datetime
 from navtech_msgs.msg import RadarBScanMsg
 import numpy as np
-# import math
 import cv2
 from sensor_msgs.msg import Imu
 from sensor_msgs_py import point_cloud2
@@ -45,8 +44,7 @@ class BagToDir(Node):
         self.rs_lidar_bin_dir = os.path.join(output_dir, 'rs_lidar') # one for rs_lidar
         os.makedirs(self.rs_lidar_bin_dir, exist_ok=True)
 
-        # self.gt_file = os.path.join(output_dir, 'gps_cartesian.txt')
-        # I actually have two imus as well lol
+        # I actually have two imus as well 
         self.vn100_imu_file = open(os.path.join(output_dir, 'vn100_imu.csv'), 'w')
         self.vn100_imu_file.write("timestamp,ang_vel_x,ang_vel_y,ang_vel_z,lin_acc_x,lin_acc_y,lin_acc_z\n")
 
@@ -58,10 +56,6 @@ class BagToDir(Node):
 
         # I need to do the audio file which is a lossless wav file TODO
 
-
-        # self.init_x = 0
-        # self.init_y = 0
-        # self.init_z = 0
         self.first_msg = True
         self.read_bag()
 
@@ -88,15 +82,9 @@ class BagToDir(Node):
                 continue
 
         while reader.has_next():
-            # temporarily limit the number of messages read for testing
-            # if self.reading_cnt == 5:
-            #     break
-            # self.reading_cnt += 1
-
             topic_name, data, t = reader.read_next()
             if topic_name in msg_type_map:
                 try:
-                    # print(f"Processing topic: {topic_name} with type: {msg_type_map[topic_name]}")
                     msg_type = msg_type_map[topic_name]
                     msg = deserialize_message(data, msg_type)
                     
@@ -104,14 +92,12 @@ class BagToDir(Node):
                         self.save_radar_image(msg)
 
                     if isinstance(msg, point_cloud2.PointCloud2): # but there are two lidar topics
-                        # print(f"Processing point cloud from topic: {topic_name}")
                         if topic_name.startswith('/lslidar128/points'):
                             self.save_lidar_bins(msg,self.ls_lidar_bin_dir)
                         elif topic_name.startswith('/rslidar128/points'):
                             self.save_lidar_bins(msg,self.rs_lidar_bin_dir)
 
                     if isinstance(msg, Imu):
-                        # print(f"Processing IMU data from topic: {topic_name}")
                         if topic_name.startswith('/vn100/data_raw'):
                             ts = float(msg.header.stamp.sec) + msg.header.stamp.nanosec * 1e-9
                             ang_vel = msg.angular_velocity
@@ -127,10 +113,10 @@ class BagToDir(Node):
                         
                 except Exception as e:
                     self.get_logger().error(f'Error processing message: {str(e)}')
+        # Close all files after processing            
+        self.mti30_imu_file.close()
+        self.vn100_imu_file.close()
                 
-    
-
-        # self.outfile.close()
 
     def save_radar_image(self, msg):
         try:
@@ -170,12 +156,9 @@ class BagToDir(Node):
     def save_lidar_bins(self, msg, lidar_bin_dir):
         try:
             timestamp = msg.header.stamp.sec * 1e9 + msg.header.stamp.nanosec
-            # for field in msg.fields:
-            #     print(f"Field name: {field.name}, offset: {field.offset}, datatype: {field.datatype}")
 
             cloud_points = list(point_cloud2.read_points(
                 msg, field_names=('x', 'y', 'z', 'intensity','ring','timestamp'), skip_nans=True))
-            # print("cloud points length: ", len(cloud_points))
             points = np.array(cloud_points, dtype=[
                                                     ('x', np.float32),
                                                     ('y', np.float32),
@@ -192,14 +175,24 @@ class BagToDir(Node):
 
     # def save_rectified_camera_imgs(self, msg, camera_dir):
     #     pass
-                     
+
+    # input: distortion parameters are in the camera_info message but only for the z???? (start with the zed x camera)
+    # for the basler ace2 camera I need to use the fisheye model, we need to do the calibration ourselves
+
+    # only dis
+
+
+    # def save_microphone_audio(self, msg, audio_dir):
+    #     pass 
+    # prefer split in secs
+    # split in msgs or in secs???
 
 
 def main(args=None):
       # Argument parser setup
     parser = argparse.ArgumentParser(description='Convert MCAP ROS2 bag to radar/lidar images and data.')
     parser.add_argument(
-        '--bag_file',
+        '--input',
         type=str,
         default='/home/samqiao/ASRL/fomo-public-sdk/raw_fomo_rosbags/deployment4/red/red.mcap',
         help='Path to the MCAP or DB3 bag file'
@@ -219,13 +212,13 @@ def main(args=None):
     rclpy.init()
     
     try:
-        MCAP2IJRR = BagToDir(args.bag_file, args.output_dir)
+        MCAP2IJRR = BagToDir(args.input, args.output_dir)
         rclpy.spin(MCAP2IJRR)
     except Exception as e:
         print(f'Error: {str(e)}')
     finally:
         rclpy.shutdown()
-    print("Done!")
+    print("Conversion Done!")
 
 if __name__ == '__main__':
     main()
