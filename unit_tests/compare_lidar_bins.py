523ee24ce8d0a942263ebe4f611cec15925de8bf
PioneerSam <pioneerqxy@gmail.com>
2025-08-25 15:35:14 -0400

    identical lidar output as matej

--- unit_tests/compare_lidar_bins.py
+++ unit_tests/compare_lidar_bins.py
@@ -25,8 +25,8 @@ def setup_logger(log_path: str):
 
 
 DEFAULT_OUTPUT_ROOT   = os.path.join(os.getcwd(), "output")
-DEFAULT_SOLUTION_ROOT = os.path.join(os.getcwd(), "solution", "red3")
-DEFAULT_VERIFY_RSLIDAR = False  # True: rslidar128, False: lslidar128
+DEFAULT_SOLUTION_ROOT = os.path.join(os.getcwd(), "solution")
+DEFAULT_VERIFY_RSLIDAR = True # True: rslidar128, False: lslidar128
 
 POINT_DTYPE = np.dtype([
     ('x',         np.float32),
